241c71d899a0db89928c6414089f3c89b7f4ecab
Zizwar <zizwar@gmail.com>
2023-01-21 03:32:20 +0000

    fix executablePath

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pptr-stream-yt
 puppeteer stream youtube
+
+1. install
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+
+2. 
+ executablePath: '/usr/bin/google-chrome-stable'
--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-const { launch, getStream } = require("puppeteer-stream");
-const {executablePath} = require('puppeteer')
-const fs = require("fs");
+const puppeteer = require('puppeteer');
+const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
 
-const file = fs.createWriteStream(__dirname + "/test.webm");
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome-stable',
+  });
+  const page = await browser.newPage();
+  const recorder = new PuppeteerScreenRecorder(page);
+  await recorder.start('./report/video/simple.mp4'); // supports extension - mp4, avi, webm and mov
+  await page.goto('https://bing.com');
 
-async function test() {
-	const browser = await launch({
-		defaultViewport: {
-			width: 1920,
-			height: 1080,
-            executablePath,
-		},
-	});
-
-	const page = await browser.newPage();
-	await page.goto("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-	const stream = await getStream(page, { audio: true, video: true });
-	console.log("recording");
-
-	stream.pipe(file);
-	setTimeout(async () => {
-		await stream.destroy();
-		file.close();
-		console.log("finished");
-	}, 1000 * 10);
-}
-
-test();
+  await page.goto('https://google.com');
+  await recorder.stop();
+  await browser.close();
+})();
