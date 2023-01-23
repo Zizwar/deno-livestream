12f6bd761328bd2bc18342695aa8e1c32332bab6
Zizwar <zizwar@gmail.com>
2023-01-23 17:36:44 +0000

    recoding chart btc 1s and zoom elm

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ puppeteer stream youtube
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt update && sudo apt install ./google-chrome-stable_current_amd64.deb
 
+////if probelm Missing Xserveer or $DISPLAY
+export DISPLAY=:1 && /usr/bin/google-chrome-stable
 2. 
 executablePath: '/usr/bin/google-chrome-stable'
 ```
--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const puppeteer = require("puppeteer");
 const { PuppeteerScreenRecorder } = require("puppeteer-screen-recorder");
 
 //
-const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+const { log } = console;
+const delay = (milliseconds) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
+//
+
+const TIME_AWAIT_NAVIGATE = 12; //second
+const TIME_AWAIT_RECORD = 190;
 //
 (async () => {
   const browser = await puppeteer.launch({
@@ -15,11 +21,13 @@ const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, mil
   const page = await browser.newPage();
   const recorder = new PuppeteerScreenRecorder(page);
 
-  console.log("recording");
+  log("start recording");
   await recorder.start("./out/video/simple.mp4"); // supports extension - mp4, avi, webm and mov
   //await page.goto('https://bing.com');
 
-  await page.goto("https://www.binance.com/en/trade/BTC_USDT?layout=pro",{waitUntil: 'domcontentloaded'});
+  await page.goto("https://www.binance.com/en/trade/BTC_USDT?layout=pro", {
+    waitUntil: "domcontentloaded",
+  });
 
   //#onetrust-accept-btn-handler     accept cookies
   //.css-4rbxuz  skip help
@@ -32,30 +40,33 @@ const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, mil
   //await page.waitFor(2000);
   // await Promise.all([
   //page.waitForNavigation(); // The promise resolves after navigation has finished
-  await delay(5000)
+  await delay(TIME_AWAIT_NAVIGATE * 1000);
 
   page.click("#onetrust-accept-btn-handler"); // Clicking the link will indirectly cause a navigation
 
   //await page.click("#onetrust-accept-btn-handler");
-  console.log("accept cookies");
+  log("accept cookies");
 
   //await page.waitFor(100);
-  await delay(2000)
+  await delay(2000);
   await page.click(".css-4rbxuz");
   //await page.waitForTimeout(5000);
   //await page.waitForTimeout(1000);
   await page.click(".css-1sh2brw");
-  console.log("skip help");
+  log("skip help");
 
   //await page.waitForTimeout(1000);
   await page.click(".css-1pj8e72");
   //await page.waitForTimeout(1000);
   await page.click(".css-1pj8e72");
-  console.log("chart 1s");
+  log("chart 1s");
+  delay(200);
+  await page.evaluate(() => (document.body.style.zoom = 1.5));
+  log("zoom chart");
 
   setTimeout(async () => {
     await recorder.stop();
     await browser.close();
-    console.log("finished");
-  }, 1000 * 15);
+    log("finished recording");
+  }, TIME_AWAIT_RECORD * 1000);
 })();
