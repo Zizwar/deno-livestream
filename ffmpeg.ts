5f7d747d3c7c70535b48b06c6dce3cb273f9dc6f
Zizwar <zizwar@gmail.com>
2023-02-03 04:51:33 +0000

    ffmpeg oiutput video from jpg

--- ffmpeg.ts
+++ ffmpeg.ts
@@ -1,7 +1,8 @@
 import process from "./process.ts";
 
 const args =
-  `ffmpeg -framerate 1 -i ai(%d).jpg -c:v libx264 -r 30 output.mp4`.split(" ");
+  `ffmpeg -framerate 1/6 -pattern_type glob -i ./static/images/*.jpg -i ./static/sounds/ai.mp3 -shortest -c:v libx264 -r 30 -pix_fmt yuv420p output6.mp4`.split(" ");
 const cmd = [...args];
 const ff = await process({ cmd });
 console.log(ff);
+//console.log(ff.error);
